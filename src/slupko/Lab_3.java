a74f348d19f13db521a561ab2730f8e8cde27d5a
Vlad Slupko <vad.slupko.ukr@gmail.com>
2019-09-26 22:31:29 +0300

    3 лаба, пофіксив
    там просто wordsInData.add(word); стояло не там де треба

--- src/slupko/Lab_3.java
+++ src/slupko/Lab_3.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 class Main{
     public static void main(String[] args) {
+        
         int number = 8117;
         int C3 = number % 3;
         int C17 = number % 17;
@@ -19,39 +20,18 @@ class Main{
         */
         String[] Names = {"Number", "C3", "C17"};
         int[] Nums = {number, C3, C17};
+
         for(int i = 0; i < (Names.length); i++){
             System.out.println(Names[i] + " = " + Nums[i]);
         }
+
         System.out.println();
-        /* 
-         * Reading File
-        */
-        // String filePath = "Lab_3_text.txt";
-        // String data;
-        // try{
-        //     data = Files.readString(Paths.get(filePath));
-        //     System.out.println("Here is data:");
-        //     System.out.println(data);
-        //     System.out.println("Data end!!!");
-        // }catch(IOException e){
-        //     e.printStackTrace();
-        // }
-
-        /* 
-         * Sorting
-        */
-
-        String data = "1k 2kK 3KkK 4Kkkk 5KkkKK 6KkKkKk 7KkkKKKK";
+       
+        String data = "1k 2kK 3KkK 4Kkkk  0sssa 546asdadsa 3kkK 5KkkKK 6KkKkKk asbbbbbb1213 7KkkKKKK";
         System.out.println("String is: ");
         System.out.println(data);
         System.out.println();
-
-        // char space = ' ';
-        // char dot = '.';
-        // char coma = ',';
-        // char quation = '?';
-        // char attention = '!';
-        
+ 
         // Must found
         char K = 'K';
         char k = 'k';
@@ -62,17 +42,21 @@ class Main{
         String[] words = data.split(" ");
 
         for(String word : words){
-            wordsInData.add(word);
             // System.out.println(word);
             nums = 0;
+
             for(int i = 0; i < word.length(); i++){
+
                 if(word.charAt(i) == k || word.charAt(i) == K){
+
                     nums++;
                     // System.out.println(nums);
                 }
             }
+
             if(nums != 0){
                 numbers.add(nums);
+                wordsInData.add(word);
             }
         }
         // System.out.println("__");
@@ -88,11 +72,9 @@ class Main{
                 break;
             }
         }
+
         for(Object elem : sorted){
             System.out.println(elem);
         }
-        
-
-
     }
 }
